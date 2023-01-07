f6f9eb6912804ba02558678609679d6672c9efc4
abhina7 <63799538+abhina7@users.noreply.github.com>
2023-01-08 00:15:58 +0530

    Add files via upload

---
+++ Javascript Basics/4.js
@@ -0,0 +1,12 @@
+let person = {
+    firstname: 'John',
+    lastname: 'Doe'
+};
+
+// First way
+console.log(person.firstname);
+console.log(person.lastname);
+
+// Second way
+console.log(person['firstname']);
+console.log(person['lastname']);
